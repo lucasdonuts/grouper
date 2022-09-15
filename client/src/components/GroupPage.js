3153ffe500e56e7da9c66526d61fe9b655a33e1b
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-15 15:47:35 -0500

    Attempting refresh bugfix

--- client/src/components/GroupPage.js
+++ client/src/components/GroupPage.js
@@ -62,10 +62,6 @@ const GroupPage = ({ currentUser, updateCurrentUser }) => {
   const handleLeaveGroup = () => {
     fetch(`/user_groups/${group.id}`, {
       method: "DELETE",
-      // headers: { 'Content-Type': 'application/json' },
-      // body: JSON.stringify({
-      //   users: group.users.filter( user => user.id !== currentUser.id )
-      // })
     }).then((res) => {
       if (res.ok) {
         const user = {
@@ -96,7 +92,6 @@ const GroupPage = ({ currentUser, updateCurrentUser }) => {
       type="button"
       className="mb-2 px-4 bg-blue-600 h-10 w-50 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
     >
-      {/* { group.users.includes(currentUser) ? "Leave group" : "Join group" } */}
       Join Group
     </button>
   );
@@ -107,7 +102,6 @@ const GroupPage = ({ currentUser, updateCurrentUser }) => {
       type="button"
       className="mb-2 px-4 bg-red-600 h-10 w-50 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
     >
-      {/* { group.users.includes(currentUser) ? "Leave group" : "Join group" } */}
       Leave Group
     </button>
   );
@@ -167,39 +161,3 @@ const GroupPage = ({ currentUser, updateCurrentUser }) => {
 };
 
 export default GroupPage;
-
-{
-  /* <section className="relative bg-white">
-  <img
-    className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
-    src={ group.image_url }
-    alt={ group.name }
-  />
-
-  <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent"></div>
-
-  <div className="relative px-4 py-32 mx-auto max-w-screen-xl lg:h-screen lg:items-center lg:flex">
-    <div className="max-w-xl text-center sm:text-left">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
-        <strong className="font-extrabold text-rose-700 sm:block">
-          { group.name }
-        </strong>
-      </h1>
-
-      <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
-        { group.description }
-      </p>
-
-      <div className="flex flex-wrap mt-8 text-center gap-4">
-        <a className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-rose-600 sm:w-auto active:bg-rose-500 hover:bg-rose-700 focus:outline-none focus:ring" href="/get-started">
-          Get Started
-        </a>
-
-        <a className="block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-rose-600 sm:w-auto hover:text-rose-700 active:text-rose-500 focus:outline-none focus:ring" href="/about">
-          Learn More
-        </a>
-      </div>
-    </div>
-  </div>
-</section> */
-}
