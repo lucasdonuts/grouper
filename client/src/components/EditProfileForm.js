d6b5d619a43a70424218465554df00650537c7fe
Lucas Duncan <lucasdonuts@gmail.com>
2022-10-28 12:25:34 -0500

    Bio input now wraps text

--- client/src/components/EditProfileForm.js
+++ client/src/components/EditProfileForm.js
@@ -87,9 +87,16 @@ const EditProfileForm = ({ user, onSubmitEdit, errors }) => {
           {/* Bio */}
           <div className="flex flex-col mb-2">
             <div className=" relative ">
-              <input
+              {/* <input
                 onChange={handleChange}
                 type="text"
+                name="bio"
+                className="h-40 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                placeholder="Tell us a little about yourself..."
+                value={ formData.bio }
+              /> */}
+              <textarea
+                onChange={handleChange}
                 name="bio"
                 className="h-40 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
                 placeholder="Tell us a little about yourself..."
